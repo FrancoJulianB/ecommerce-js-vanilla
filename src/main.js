2980ba313d09d97da1e175ccbb809d8b8f0426b0
Franco Barrera <franco.barrera@istea.com.ar>
2026-06-26 18:02:55 +0200

    [FEAT] Add product catalog UI

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { getProducts } from "./services/product.service.js";
 import { appState } from "./state/app.state.js";
+import { renderProducts } from "./ui/product.ui.js";
 
 async function initializeApplication() {
     appState.products = await getProducts();
     appState.filteredProducts = [...appState.products];
-    console.table(appState.products);
+    renderProducts(appState.filteredProducts);
 }
 
 initializeApplication();
---
+++ src/ui/product.ui.js
@@ -0,0 +1,42 @@
+const productsContainer = document.querySelector("#productsContainer");
+
+function createProductCard(product) {
+    return `
+        <article class="col-12 col-md-6 col-lg-4">
+            <div class="card product-card h-100 shadow-sm">
+
+                <img
+                    src="${product.image}"
+                    class="card-img-top product-card__image"
+                    alt="${product.title}"
+                />
+
+                <div class="card-body d-flex flex-column">
+
+                    <h5 class="card-title">
+                        ${product.title}
+                    </h5>
+
+                    <p class="fw-bold fs-5 text-primary mt-auto">
+                        $${product.price}
+                    </p>
+
+                    <button
+                        class="btn btn-primary mt-2"
+                        data-product-id="${product.id}"
+                    >
+                        View details
+                    </button>
+
+                </div>
+
+            </div>
+        </article>
+    `;
+}
+
+export function renderProducts(products) {
+    productsContainer.innerHTML = products
+    .map(createProductCard)
+    .join("");
+}
